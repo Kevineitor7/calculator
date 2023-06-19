0a32e5c918892716d3da99f1ceae8b16c93c08ce
Kevin <qeven03@gmail.com>
2023-06-19 13:25:59 -0500

    added key event, backspace  and final css details

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const plus = document.querySelector('.key.plus')
 const minus = document.querySelector('.key.minus')
 const multiplication = document.querySelector('.key.multiply')
 const division = document.querySelector('.key.divide')
-const root = document.querySelector('.key.root')
-const remainder = document.querySelector('.key.remainder')
+const back = document.querySelector('.key.backspace')
 const dot = document.querySelector('.key.dot')
+const key = document.querySelector('.key')
 
 let x = ''
 let y = ''
@@ -26,256 +26,24 @@ let operator = []
 let change = false
 let resultado 
 
-one.addEventListener('click', () => {
-    calculation.innerHTML += 1
-    if (change == true) {
-        y += '1'
-        console.log(x,y)
-
-    } else {
-        x += '1'
-        console.log(x,y)
-    }
-})
-
-two.addEventListener('click', () => {
-    calculation.innerHTML += 2
-    if (change == true) {
-        y += '2'
-        console.log(x,y)
-    } else {
-        x += '2'
-        console.log(x,y)
-    }
-})
-
-three.addEventListener('click', () => {
-    calculation.innerHTML += 3
-    if (change == true) {
-        y += '3'
-        console.log(x,y)
-    } else {
-        x += '3'
-        console.log(x,y)
-    }
-})
-
-four.addEventListener('click', () => {
-    calculation.innerHTML += 4
-    if (change == true) {
-        y += '4'
-        console.log(x,y)
-    } else {
-        x += '4'
-        console.log(x,y)
-    }
-})
-
-five.addEventListener('click', () => {
-    calculation.innerHTML += 5
-    if (change == true) {
-        y += '5'
-        console.log(x,y)
-    } else {
-        x += '5'
-        console.log(x,y)
-    }
-})
-
-six.addEventListener('click', () => {
-    calculation.innerHTML += 6
-    if (change == true) {
-        y += '6'
-        console.log(x,y)
-    } else {
-        x += '6'
-        console.log(x,y)
-    }
-})
-
-seven.addEventListener('click', () => {
-    calculation.innerHTML += 7
-    if (change == true) {
-        y += '7'
-        console.log(x,y)
-    } else {
-        x += '7'
-        console.log(x,y)
-    }
-})
-
-eight.addEventListener('click', () => {
-    calculation.innerHTML += 8
-    if (change == true) {
-        y += '8'
-        console.log(x,y)
-    } else {
-        x += '8'
-        console.log(x,y)
-    }})
-
-nine.addEventListener('click', () => {
-    calculation.innerHTML += 9
-    if (change == true) {
-        y += '9'
-        console.log(x,y)
-    } else {
-        x += '9'
-        console.log(x,y)
-    }
-})
-
-zero.addEventListener('click', () => {
-    calculation.innerHTML += 0
-    if (change == true) {
-        y += '0'
-        console.log(x,y)
-    } else {
-        x += '0'
-        console.log(x,y)
-    }
-})
-
-dot.addEventListener('click', () => {
-    calculation.innerHTML += '.'
-    if (change == true) {
-        y += '.'
-        console.log(x,y)
-    } else {
-        x += '.'
-        console.log(x,y)
-    }
-})
-
-clear.addEventListener('click', () => {
-    calculation.innerHTML = ''
-    result.innerHTML = ''
-    x = ''
-    y = ''
-    operator = []
-    change = false
-    console.clear()
-})
-
-plus.addEventListener('click', () => {
-    calculation.innerHTML += '+'
-    if (change == false) {
-        change = true
-    } else if (change == true) {
-        if (operator[operator.length - 1] == 'sum') {
-            add(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'sub') {
-            subtract(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'mul') {
-            multiply(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'div') {
-            divide(Number(x), Number(y))
-            x = resultado
-            y = 0
-        }
-    }
-    operator.push('sum')
-    console.log(operator)
-})
-
-minus.addEventListener('click', () => {
-    calculation.innerHTML += '-'
-    if (change == false) {
-        change = true
-    } else if (change == true) {
-        if (operator[operator.length - 1] == 'sum') {
-            add(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'sub') {
-            subtract(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'mul') {
-            multiply(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'div') {
-            divide(Number(x), Number(y))
-            x = resultado
-            y = 0
-        }
-    }
-    operator.push('sub')
-    console.log(operator)
-})
-
-multiplication.addEventListener('click', () => {
-    calculation.innerHTML += '*'
-    if (change == false) {
-        change = true
-    } else if (change == true) {
-        if (operator[operator.length - 1] == 'sum') {
-            add(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'sub') {
-            subtract(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'mul') {
-            multiply(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'div') {
-            divide(Number(x), Number(y))
-            x = resultado
-            y = 0
-        }
-    }
-    operator.push('mul')
-    console.log(operator)
-})
-
-division.addEventListener('click', () => {
-    calculation.innerHTML += '/'
-    if (change == false) {
-        change = true
-    } else if (change == true) {
-        if (operator[operator.length - 1] == 'sum') {
-            add(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'sub') {
-            subtract(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'mul') {
-            multiply(Number(x), Number(y))
-            x = resultado
-            y = 0
-        } else if (operator[operator.length - 1] == 'div') {
-            divide(Number(x), Number(y))
-            x = resultado
-            y = 0
-        }
-    }
-    operator.push('div')
-    console.log(operator)
-})
-
-root.addEventListener('click', () => {
-    calculation.innerHTML += '√'
-})
-
-remainder.addEventListener('click', () => {
-    calculation.innerHTML += '%'
-})
-
-equal.addEventListener('click', () => {
-       operate(operator[operator.length - 1], Number(x), Number(y))
-})
+one.addEventListener('click', uno)
+two.addEventListener('click', dos)
+three.addEventListener('click', tres)
+four.addEventListener('click', cuatro)
+five.addEventListener('click', cinco)
+six.addEventListener('click', seis)
+seven.addEventListener('click', siete)
+eight.addEventListener('click', ocho)
+nine.addEventListener('click', nueve)
+zero.addEventListener('click', cero)
+dot.addEventListener('click', punto)
+clear.addEventListener('click', ac)
+plus.addEventListener('click', suma)
+minus.addEventListener('click', resta)
+multiplication.addEventListener('click', multiplicacion)
+division.addEventListener('click', dividir)
+equal.addEventListener('click', igual)
+back.addEventListener('click', backspace) 
 
 function add(a, b) {
     resultado = a + b
@@ -314,5 +82,298 @@ function operate(op, a, b) {
     }
 }
 
+document.addEventListener('keydown', (e) => {
+    if (e.key == 1) { uno() } 
+    else if (e.key == 2) { dos() }
+    else if (e.key == 3) { tres() }
+    else if (e.key == 4) { cuatro() }
+    else if (e.key == 5) { cinco() }
+    else if (e.key == 6) { seis() }
+    else if (e.key == 7) { siete() }
+    else if (e.key == 8) { ocho() }
+    else if (e.key == 9) { nueve() }
+    else if (e.key == 0) { cero() }
+    else if (e.key == '.') { punto() }
+    else if (e.key == 'c' || e.key == 'C') { ac() }
+    else if (e.key == '+') { suma() }
+    else if (e.key == '-') { resta() }
+    else if (e.key == '*') { multiplicacion() }
+    else if (e.key == '/') { dividir() }
+    else if (e.key == '=' || e.key == 'Enter') { igual() }
+    else if (e.key == 'Backspace') { backspace()}
+  });
 
+function uno() {
+    calculation.innerHTML += 1
+    if (change == true) {
+        y += '1'
+        console.log(x,y)
+    } else {
+        x += '1'
+        console.log(x,y)
+    }
+}
+
+function dos() {
+    calculation.innerHTML += 2
+    if (change == true) {
+        y += '2'
+        console.log(x,y)
+    } else {
+        x += '2'
+        console.log(x,y)
+    }
+}
+
+function tres() {
+    calculation.innerHTML += 3
+    if (change == true) {
+        y += '3'
+        console.log(x,y)
+    } else {
+        x += '3'
+        console.log(x,y)
+    }
+}
+
+function cuatro() {
+    calculation.innerHTML += 4
+    if (change == true) {
+        y += '4'
+        console.log(x,y)
+    } else {
+        x += '4'
+        console.log(x,y)
+    }
+}
+
+function cinco() {
+    calculation.innerHTML += 5
+    if (change == true) {
+        y += '5'
+        console.log(x,y)
+    } else {
+        x += '5'
+        console.log(x,y)
+    }
+}
+
+function seis() {
+    calculation.innerHTML += 6
+    if (change == true) {
+        y += '6'
+        console.log(x,y)
+    } else {
+        x += '6'
+        console.log(x,y)
+    }
+}
+
+function siete() {
+    calculation.innerHTML += 7
+    if (change == true) {
+        y += '7'
+        console.log(x,y)
+    } else {
+        x += '7'
+        console.log(x,y)
+    }
+}
+
+function ocho() {
+    calculation.innerHTML += 8
+    if (change == true) {
+        y += '8'
+        console.log(x,y)
+    } else {
+        x += '8'
+        console.log(x,y)
+    }
+}
+
+function nueve() {
+    calculation.innerHTML += 9
+    if (change == true) {
+        y += '9'
+        console.log(x,y)
+    } else {
+        x += '9'
+        console.log(x,y)
+    }
+}
+
+function cero() {
+    calculation.innerHTML += 0
+    if (change == true) {
+        y += '0'
+        console.log(x,y)
+    } else {
+        x += '0'
+        console.log(x,y)
+    }
+}
+
+function punto() {
+    calculation.innerHTML += '.'
+    if (change == true) {
+        y += '.'
+        console.log(x,y)
+    } else {
+        x += '.'
+        console.log(x,y)
+    }
+}
+
+function ac() {
+    calculation.innerHTML = ''
+    result.innerHTML = ''
+    x = ''
+    y = ''
+    operator = []
+    change = false
+    console.clear()
+}
+
+function backspace() {    
+    result.textContent = ''
+    
+    if (calculation.innerHTML.substring(calculation.innerHTML.length - 1) == '+' ||
+    calculation.innerHTML.substring(calculation.innerHTML.length - 1) == '-' ||
+    calculation.innerHTML.substring(calculation.innerHTML.length - 1) == '*' ||
+    calculation.innerHTML.substring(calculation.innerHTML.length - 1) == '/') {
+        operator.pop()
+    } 
+
+calculation.innerHTML = calculation.innerHTML.substring(0, calculation.innerHTML.length - 1)
+
+    if (change == true) {
+        y = y.substring(0, y.length - 1)
+        console.log(x,y)
+    } else if (change == false) {
+        x = x.substring(0, x.length - 1)
+        console.log(x,y)
+    }
+
+    if (operator.length == 0) {
+        change = false
+        console.log(change)
+        x = calculation.innerHTML
+    }
+
+    if (y.length == 0 && operator.length >= 1) {
+        x = eval(calculation.innerHTML.substring(0, calculation.innerHTML.length - 1))
+    }
+}
+
+function suma() {
+    calculation.innerHTML += '+'
+    if (change == false) {
+        change = true
+    } else if (change == true) {
+        if (operator[operator.length - 1] == 'sum') {
+            add(Number(x), Number(y))
+            x = resultado
+            y = ''
+        } else if (operator[operator.length - 1] == 'sub') {
+            subtract(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'mul') {
+            multiply(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'div') {
+            divide(Number(x), Number(y))
+            x = resultado
+            y = 0
+        }
+    }
+    operator.push('sum')
+    console.log(operator)
+}
+
+function resta() {
+    calculation.innerHTML += '-'
+    if (change == false) {
+        change = true
+    } else if (change == true) {
+        if (operator[operator.length - 1] == 'sum') {
+            add(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'sub') {
+            subtract(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'mul') {
+            multiply(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'div') {
+            divide(Number(x), Number(y))
+            x = resultado
+            y = 0
+        }
+    }
+    operator.push('sub')
+    console.log(operator)
+}
+
+function multiplicacion() {
+    calculation.innerHTML += '*'
+    if (change == false) {
+        change = true
+    } else if (change == true) {
+        if (operator[operator.length - 1] == 'sum') {
+            add(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'sub') {
+            subtract(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'mul') {
+            multiply(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'div') {
+            divide(Number(x), Number(y))
+            x = resultado
+            y = 0
+        }
+    }
+    operator.push('mul')
+    console.log(operator)
+}
+
+function dividir() {
+    calculation.innerHTML += '/'
+    if (change == false) {
+        change = true
+    } else if (change == true) {
+        if (operator[operator.length - 1] == 'sum') {
+            add(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'sub') {
+            subtract(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'mul') {
+            multiply(Number(x), Number(y))
+            x = resultado
+            y = 0
+        } else if (operator[operator.length - 1] == 'div') {
+            divide(Number(x), Number(y))
+            x = resultado
+            y = 0
+        }
+    }
+    operator.push('div')
+    console.log(operator)
+}
+
+function igual() {
+    operate(operator[operator.length - 1], Number(x), Number(y))
+}
 
